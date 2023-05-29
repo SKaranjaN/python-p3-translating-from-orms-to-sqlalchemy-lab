817c0e028fbe5745be40291b34d58b03bdc03b57
Stephen <skaranja654@gmail.com>
2023-05-29 19:29:28 +0300

    Completed assignment

--- lib/dog.py
+++ lib/dog.py
@@ -1,22 +1,44 @@
+from sqlalchemy import create_engine
 from models import Dog
+from sqlalchemy.ext.declarative import declarative_base
 
-def create_table(base):
-    pass
+base = declarative_base()
+
+engine = create_engine('sqlite:///:memory:')
+
+def create_table(base, engine):
+    sql = """
+        CREATE TABLE IF NOT EXISTS dogs (
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            breed TEXT
+        );
+    """
+    with engine.connect() as connection:
+        connection.execute(sql)
 
 def save(session, dog):
-    pass
+    session.add(dog)
+    session.commit()
+
+    return session
+
+def new_from_db(session, row):
+    return session.query(Dog).filter_by(id = row.id).first()
 
 def get_all(session):
-    pass
+    return session.query(Dog)
 
 def find_by_name(session, name):
-    pass
+    return session.query(Dog).filter_by(name = name).first()
 
 def find_by_id(session, id):
-    pass
+    return session.query(Dog).filter_by(id = id).first()
 
 def find_by_name_and_breed(session, name, breed):
-    pass
+    return session.query(Dog).filter_by(name = name, breed = breed).first()
 
 def update_breed(session, dog, breed):
-    pass
+    dog.breed = breed
+    session.commit()
+    return session
